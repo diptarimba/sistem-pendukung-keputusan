77df113a14451a15b6e5ce135b74b42939240573
Dipta <dipta.harimbawa@jatis.com>
2023-08-04 12:43:04 +0700

    [fix] seeder

--- database/seeders/SymptomSeeder.php
+++ database/seeders/SymptomSeeder.php
@@ -17,38 +17,23 @@ class SymptomSeeder extends Seeder
     {
         $symptomCategory = [
             [
-                "name" => 'Kelelahan'
+                "name" => 'Pengecekan Etrus'
             ],[
-                "name" => 'Obesitas'
+                "name" => 'Pengecekan Kesehatan Reproduksi & Body Condition Score'
+            ],[
+                "name" => 'Pengamatan Siklus Reproduksi (Fase Pre Etrus, Etrus, Men Etrus, Dietrus)'
             ]
         ];
         SymptomCategory::insert($symptomCategory);
         $data =[
-            "Nafsu makan berkurang",
-            "Nafas sesak / megap-megap",
-            "Nafas ngorok basah",
-            "Bersin-bersin",
-            "Batuk",
-            "Kerabang telur pucat",
-            "Bulu kusam dan berkerut",
-            "Diare",
-            "Produksi telur menurun",
-            "Kedinginan",
-            "Tampak lesu",
-            "Mencret kehijau-hijauan",
-            "Mencret keputih-putihan",
-            "Muka pucat",
-            "Nampak membiru",
-            "Pembengkakan pial",
-            "Jengger pucat",
-            "Kaki dan sayap lumpuh",
-            "Keluar cairan dari mata dan hidung",
-            "Kepala bengkak",
-            "Kepala terputar",
-            "Pembengkakan dari sinus dan mata",
-            "Perut membesar",
-            "Sayap menggantung",
-            "Terdapat kotoran putih menempel disekitar anus",
+            "Untuk si betina, apakah ada pemerahan pada vulva?",
+            "Apakah si betina memiliki kecenderungan terhadap si pejantan?",
+            "Apakah perilaku hewan mulai berubah seperti gelisah, mencari perhatian lebih, atau lebih aktif secara fisik?",
+            "Apakah ada peningkatan bengkak pada vulva atau keluarnya lendir dari vulva?",
+            "Apakah perilaku hewan berubah menjadi agresif?",
+            "Apakah nafsu makan menurun tetapi dalam kondisi sehat?",
+            "Apakah ekor berbentuk huruf 'O' ?",
+            "Apakah sapi sering melenguh panjang?",
         ];
         foreach($data as $each){
             Symptom::create([
@@ -58,24 +43,25 @@ class SymptomSeeder extends Seeder
         }
 
         $data =[
-            "Mati secara mendadak",
-            "Kerabang telur kasar",
-            "Putih Telur Encer",
-            "Kotoran kuning kehijauan",
-            "Pembengkakan daerah fasial dan sekitar mata",
-            "Kotoran atau feses berdarah",
-            "Bergerombol di sudut kandang",
-            "Mematuk daerah kloaka",
-            "Telur lebih kecil",
-            "Kelumpuhan pada tembolok",
-            "Bernafas dengan mulut sambil menjulurkan leher",
-            "Batuk berdarah",
-            "Tidur paruhnya diletakkan dilantai",
-            "Duduk dengan sikap membungkuk",
-            "Kelihatan mengantuk dengan bulu berdiri",
-            "Badan kurus",
-            "Terdapat lendir bercampur darah pada rongga mulut",
-            "Kaki pincang"
+            "Apakah hewan mengalami kelainan fisik?",
+            "Apakah hewan pernah mengalami gejala infeksi pada organ reproduksi?",
+            "Apakah ada penyumbatan pada serviks hewan?",
+            "Apakah Anda mengamati bagian tubuh lain, seperti pangkal ekor, leher, atau paha, untuk menilai kondisi tubuh hewan ternak sapi/kambing Anda secara keseluruhan?",
+            "Apakah tulang punggung atau tulang rusuk hewan ternak sapi/kambing Anda terlihat menonjol atau terasa dengan mudah saat disentuh?",
+            "Berapa berat badan perkiraan hewan ternak sapi/kambing Anda saat ini?",
+            "Berapakah umur sapi/kambing?",
+            "Apakah siklus birahi sapi terjadi selama 3 hari?",
+        ];
+        foreach($data as $each){
+            Symptom::create([
+                'name' => $each,
+                'category_id' => 2
+            ]);
+        }
+
+        $data =[
+            "Apakah terdapat lendir pada organ kelamin betina?",
+            "Apakah vulva belum membengkakdan belum merah, jika dilihat pada bagian dalam vulva masih berwarna merah muda?",
         ];
         foreach($data as $each){
             Symptom::create([
